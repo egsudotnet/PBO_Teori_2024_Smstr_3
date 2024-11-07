13c56a014ba97af149b2f688f7a36cf2e42477a2
MSI\Egih Sugiatna <egsudotnet@gmail.com>
2024-11-07 20:47:03 +0700

    -

---
+++ classes/PersegiPanjang.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace MyApplication
+{
+  class PersegiPanjang
+  {
+    public double panjang;
+    public double lebar;
+    
+    public double Luas()
+    {
+      return panjang * lebar;
+    }
+  }
+}
